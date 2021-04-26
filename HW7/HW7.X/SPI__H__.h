857e1fb0ec6d620f3433459bd8857dfd244536bc
EDJohns <evanjohns2022@u.northwestern.edu>
2021-04-26 15:58:23 -0500

    reorginization

---
+++ HW7/HW7.X/SPI__H__.h
@@ -0,0 +1,14 @@
+/* 
+ * File:   SPI__H__.h
+ * Author: evanj
+ *
+ * Created on April 26, 2021, 1:04 AM
+ */
+
+#ifndef SPI__H__
+#define SPI__H__
+
+void initSPI();
+unsigned char spi_io(unsigned char o);
+
+#endif // SPI__H__
